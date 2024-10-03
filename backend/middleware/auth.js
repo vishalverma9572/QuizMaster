37390d16a9c86058f8ff66ce159394214c05bbe4
Aditya-NV-06 <adityanv4@gmail.com>
2024-10-03 19:54:51 +0530

    (fix)added user still exists in auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const user = require('../models/User');
+
 
 module.exports = function (req, res, next) {
     const token = req.header('x-auth-token');
@@ -6,10 +8,39 @@ module.exports = function (req, res, next) {
     console.log(token);
     if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
+
+
+     //check the user still exists
+    const find_user=()=>{
+        user.findById(req.user.id)
+        .then(foundUser => {
+            if (!foundUser) {
+                return res.status(404).json({ msg: 'User not found' });
+            }
+            next();
+        })
+        .catch(err => {
+            console.error(err.message);
+            res.status(500).json({ msg: 'Server error' });
+        });
+    }
+
+    if(req.user){
+        user.findById(req.user.id)
+        .then(user=>{ 
+            if(user.resetTokenExpiry < Date.now()){
+                return res.status(401).json({ msg: 'Token is not valid' });
+            }
+        })
+        .catch(err=>{console.log(err)})
+    }
+
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.user;
         console.log('success..',req.user);
+        find_user();
         next();
     } catch (err) {
         res.status(401).json({ msg: 'Token is not valid' });
