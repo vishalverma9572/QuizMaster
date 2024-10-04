35620dd7007ecbd91c089f3a41285946245eac37
Aditya-NV-06 <adityanv4@gmail.com>
2024-10-04 07:08:22 +0530

    refactor code

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,8 +4,7 @@ const user = require('../models/User');
 module.exports = function (req, res, next) {
     console.log("Auth middleware");
     const token = req.header('x-auth-token');
-
-    
+    // token logging
     console.log(token);
     if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
