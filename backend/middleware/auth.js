3df2f9bb5532e1b9f689706cc59b307b5a0d550c
Aditya-NV-06 <adityanv4@gmail.com>
2024-10-04 06:33:11 +0530

    solved merge conflicts

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -39,8 +39,8 @@ module.exports = function (req, res, next) {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.user;
-        console.log('success..',req.user);
         find_user();
+        console.log('success..',req.user);
         next();
     } catch (err) {
         res.status(401).json({ msg: 'Token is not valid' });
