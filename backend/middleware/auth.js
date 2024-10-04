0c9fe58dc538fc720069bb6d95014431d7a909c5
Aditya-NV-06 <adityanv4@gmail.com>
2024-10-04 07:10:27 +0530

    refactor code

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,6 @@ module.exports = function (req, res, next) {
     const token = req.header('x-auth-token');
     console.log(token);
     if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
-
     const id_user = req.user.id;
 
     // Check if the user still exists
