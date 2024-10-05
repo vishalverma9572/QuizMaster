9692334059a0a9be08e1d0224b2f054af615a9b7
Vishal Kumar <vishalverma9572@gmail.com>
2024-10-06 03:22:06 +0530

    Reverted backend to the state before merge commit b279327e

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -101,15 +101,10 @@ const updateUsername = async (req, res) => {
     }
 }
 
-// Update Password
-router.put('/update-password', auth, async (req, res) => {
-    const { password, oldPassword } = req.body;
+const updatePassword = async (req, res) => {
+    const { password } = req.body;
     try {
         const user = await User.findById(req.user.id);
-        const isMatch = await bcrypt.compare(oldPassword, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ msg: 'Invalid old password' });
-        }
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
         await user.save();
