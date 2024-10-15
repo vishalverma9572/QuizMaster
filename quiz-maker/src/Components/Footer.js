ac5ad1ebfc3bf6a5d3076912b205fa8a2aefeaee
Shreya Jaiswal <shreyaa.we@gmail.com>
2024-10-15 10:48:03 +0530

    Added the footer

---
+++ quiz-maker/src/Components/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './Footer.css';
+// import Logo from "../Assets/Logo.svg";
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { BsYoutube } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section-one">
+        {/* <div className="footer-logo-container">
+          <img src={Logo} alt="" />
+        </div> */}
+        <div className="footer-icons">
+          <BsTwitter />
+          <SiLinkedin />
+          <BsYoutube />
+          <FaFacebookF />
+        </div>
+      </div>
+      <div className="footer-section-two">
+        <div className="footer-section-columns">
+          <span>Suggestions</span>
+          <span>Help</span>
+          <span>Share</span>
+          <span>Testimonials</span>
+          <span>Work</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>244-5333-7783</span>
+          <span>hello@quizmaster.com</span>
+          <span>press@quizmaster.com</span>
+          <span>contact@quizmaster.com</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>Terms & Conditions</span>
+          <span>Privacy Policy</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
